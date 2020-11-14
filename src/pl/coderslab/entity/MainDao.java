4b7c42594990d23a0aca8748f48ad016e4310813
Kruchyxe <ragamuffin@o2.pl>
2020-11-14 16:24:00 +0100

    third commit

--- src/pl/coderslab/entity/MainDao.java
+++ src/pl/coderslab/entity/MainDao.java
@@ -3,10 +3,28 @@ package pl.coderslab.entity;
 public class MainDao {
     public static void main(String[] args) {
         UserDao userDao = new UserDao();
-        User user = new User();
-        user.setUserName("Katarzyna");
-        user.setEmail("kasiapik@interia.eu");
-        user.setPassword("Bimbos123");
-        userDao.insert(user);
+//        User user = new User();
+//        user.setUserName("Alfred");
+//        user.setEmail("jabaduu@yahoo.com");
+//        user.setPassword("Inch2two");
+//        userDao.insert(user);
+
+
+        User userToUpdate = new User();
+        userToUpdate.setId(5);
+        userToUpdate.setUserName("Alfred");
+        userToUpdate.setEmail("jabaduu@yahoo.com");
+        userToUpdate.setPassword("Inch123two");
+        userDao.update(userToUpdate);
+
+//        User read = userDao.read(2); // nie działa
+//        System.out.println(read);
+
+//        userDao.delete(4);
+
+
+
+
+
     }
 }
--- src/pl/coderslab/entity/UserDao.java
+++ src/pl/coderslab/entity/UserDao.java
@@ -4,9 +4,24 @@ import java.sql.*;
 
 public class UserDao {
 
-    /**Query creating User in DB */
+    /** Query creating User in DB  */
     private static final String CREATE_USER =
             "INSERT INTO users (username, email, password) VALUES (?, ?, ?);";
+
+    /** Query read from DB by userId  */
+    private static final String READ_USER =
+            "SELECT * FROM users WHERE id = ?;";
+
+    /** Query to update User in DB  */
+    private static final String UPDATE_USER =
+            "UPDATE users SET username = ?, email = ?, password = ? WHERE id = ?;";
+
+    /** Query to delete user from DB  */
+    private static final String DELETE_USER =
+            "DELETE FROM users WHERE id = ?;";
+
+    /** */
+
 
     public User insert(User user) {
         try (Connection conn = DbUtil.getConnection()) {
@@ -24,6 +39,50 @@ public class UserDao {
         } catch (SQLException e) {
             e.printStackTrace();
             return null;
+        }
+    }
+
+    public void update (User user){
+        try (Connection conn = DbUtil.getConnection()){
+            PreparedStatement statement = conn.prepareStatement(UPDATE_USER);
+            statement.setString(1, user.getUserName());
+            statement.setString(2, user.getEmail());
+            statement.setString(3, this.hashPassword(user.getPassword()));
+            statement.setInt(4, user.getId());
+            statement.executeUpdate();
+
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+
+    public User read(int userId) {
+        try (Connection conn = DbUtil.getConnection()) {
+            PreparedStatement statement = conn.prepareStatement(READ_USER);
+            statement.setInt(1, userId);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getInt("id"));
+                user.setUserName(resultSet.getString("username"));
+                user.setEmail(resultSet.getString("email"));
+                user.setPassword(resultSet.getString("password"));
+                return user;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void delete (int userId){
+        try (Connection conn = DbUtil.getConnection()) {
+                PreparedStatement statement = conn.prepareStatement(DELETE_USER);
+                statement.setInt(1, userId);
+                statement.executeUpdate();
+        } catch (SQLException e) {
+                e.printStackTrace();
         }
     }
 
